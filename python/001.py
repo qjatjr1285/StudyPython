862d7fb1f0458603de94edc1a1d9d48d61ac5b11
qjatjr1285 <76689668+qjatjr1285@users.noreply.github.com>
2023-05-06 15:59:44 +0900

    내용 수정

--- python/001.py
+++ python/001.py
@@ -1,8 +1,9 @@
-def solution(n):
-    x = 0
-    for i in range(n):
-        if (n % 2 == 0): 
-            x += (n ** 2)
-        else:
-            x += n
-    return answer
+def solution(a, b, c):
+    cnt = 0
+    if a != b != c:
+        cnt = a + b + c
+    elif a != b == c or a == b != c or a == c != b:
+        cnt = (a + b + c) * ((a ** 2)+ (b ** 2) + (c ** 2))
+    elif a == b == c:
+        cnt = (a + b + c) * ((a ** 2)+ (b ** 2) + (c ** 2)) * ((a ** 3)+ (b ** 3) + (c ** 3))
+    return cnt
