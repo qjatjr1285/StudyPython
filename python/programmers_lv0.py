47d30a9e32e0614198a2ef604deac935ee78df0b
qjatjr1285 <76689668+qjatjr1285@users.noreply.github.com>
2023-05-06 15:59:52 +0900

    📝 문제 풀이 추가

--- python/programmers_lv0.py
+++ python/programmers_lv0.py
@@ -108,7 +108,15 @@ def solution(a, b, flag):
 
 
 # 주사위 게임 2
-
+def solution(a, b, c):
+    cnt = 0
+    if a != b != c and a != c:
+        cnt = a + b + c
+    elif a != b == c or a == b != c or a == c != b:
+        cnt = (a + b + c) * ((a ** 2)+ (b ** 2) + (c ** 2))
+    elif a == b == c:
+        cnt = (a + b + c) * ((a ** 2)+ (b ** 2) + (c ** 2)) * ((a ** 3)+ (b ** 3) + (c ** 3))
+    return cnt
 
 # 원소들의 곱과 합
 
