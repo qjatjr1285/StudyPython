b4f9637324c9ea4a1ad94a40ffaa767c9940fa1e
qjatjr1285 <76689668+qjatjr1285@users.noreply.github.com>
2023-05-08 17:35:12 +0900

    내용 수정

--- python/001.py
+++ python/001.py
@@ -1,9 +1,12 @@
-def solution(a, b, c):
-    cnt = 0
-    if a != b != c:
-        cnt = a + b + c
-    elif a != b == c or a == b != c or a == c != b:
-        cnt = (a + b + c) * ((a ** 2)+ (b ** 2) + (c ** 2))
-    elif a == b == c:
-        cnt = (a + b + c) * ((a ** 2)+ (b ** 2) + (c ** 2)) * ((a ** 3)+ (b ** 3) + (c ** 3))
-    return cnt
+def solution(num_list):
+    hol = ''
+    zzak = ''
+    n = 0
+    for i in num_list:
+        if num_list[i] % 2 == 0:
+            zzak += num_list[i]
+        else:
+            hol += num_list[i]
+    int(hol) + int(zzak)
+    
+    return n
