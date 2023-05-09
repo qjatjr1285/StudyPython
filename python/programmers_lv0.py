8946077e2b64018d10c6c527495b38b8e61500ea
qjatjr1285 <76689668+qjatjr1285@users.noreply.github.com>
2023-05-09 16:50:10 +0900

    📝 문제 풀이 추가

--- python/programmers_lv0.py
+++ python/programmers_lv0.py
@@ -190,3 +190,10 @@ def solution(num_list):
 
 
 
+# 중복된 문자 제거
+def solution(my_string):
+    result = ''
+    for i in my_string:
+        if i not in result:
+            result += i
+    return result
--- python/코딩테스트_입문.py
+++ python/코딩테스트_입문.py
@@ -1,7 +1,48 @@
-# 중복된 문자 제거
-def solution(my_string):
-    result = ''
-    for i in my_string:
-        if i not in result:
-            result += i
-    return result
+# Day 1 사칙연산
+# 두 수의 합
+def solution(num1, num2):
+    return num1 + num2
+
+# 두 수의 차
+def solution(num1, num2):
+    return num1 - num2
+
+# 두 수의 곱
+def solution(num1, num2):
+    return num1 * num2
+
+# 몫 구하기
+def solution(num1, num2):
+    return num1 // num2
+
+# Day 2 사칙연산, 조건문, 배열
+# 두 수의 나눗셈
+def solution(num1, num2):
+    return int((num1 / num2) * 1000)
+
+# 숫자 비교하기
+def solution(num1, num2):
+    return 1 if num1 == num2 else -1
+
+# 분수의 덧셈
+def gcd(a, b):
+    while b > 0:
+        a, b = b, a % b
+    return a
+
+def solution(numer1, denom1, numer2, denom2):
+    bm = denom1 * denom2
+    bj = (numer1 * denom2) + (numer2 * denom1)
+    
+    a = gcd(bm, bj)
+    
+    answer = [bj // a, bm // a]
+    return answer
+
+# 배열 두 배 만들기
+def solution(numbers):
+    answer = []
+    for i in numbers:
+        answer.append(i*2)
+    return answer
+
