d4ac399476ced1ed5791640e0c548c8566146492
qjatjr1285 <76689668+qjatjr1285@users.noreply.github.com>
2023-05-10 17:55:04 +0900

    📝 문제 풀이 추가

--- python/코딩테스트_입문.py
+++ python/코딩테스트_입문.py
@@ -46,3 +46,47 @@ def solution(numbers):
         answer.append(i*2)
     return answer
 
+# Day 3 사칙연산, 배열, 수학
+# 나머지 구하기
+def solution(num1, num2):
+    return num1 % num2
+
+# 중앙값 구하기
+def solution(array):
+    array.sort()
+    return array[len(array) // 2 ]
+
+# 최빈값 구하기
+def solution(array):
+    while len(array) != 0:
+        for i, a in enumerate(set(array)):
+            array.remove(a)
+        if i == 0: return a
+    return -1
+
+# 짝수는 싫어요
+def solution(n):
+    arr = []
+    for i in range(n+1):
+        if i % 2 != 0:
+            arr.append(i)
+    return arr
+
+
+# Day 4 수학, 배열
+# 피자 나눠 먹기(1)
+def solution(n):
+    if n % 7 == 0:
+        return n // 7
+    else:
+        return n // 7 + 1
+
+# 피자 나눠 먹기(2)
+
+
+# 피자 나눠 먹기(3)
+
+
+# 배열의 평균값
+
+
