5b324d157cd010f226a1a17d13bb203fc27b0c85
qjatjr1285 <76689668+qjatjr1285@users.noreply.github.com>
2023-05-11 17:31:26 +0900

    📝 문제 풀이 추가

--- python/코딩테스트_입문.py
+++ python/코딩테스트_입문.py
@@ -82,11 +82,59 @@ def solution(n):
         return n // 7 + 1
 
 # 피자 나눠 먹기(2)
+def solution(n):
+    if n % 6 == 0:
+        return n // 6
+    else:
+        for i in range(1, 100):
+            if (n * i) % 6 == 0:
+                return (n * i) // 6
 
 
 # 피자 나눠 먹기(3)
+def solution(slice, n):
+    if n % slice == 0:
+        return n // slice
+    else:
+        return n // slice + 1
 
+def solution(slice, n):
+    return n // slice if n % slice == 0 else n // slice + 1
 
 # 배열의 평균값
+def solution(numbers):
+    return sum(numbers) / len(numbers)
+    
 
+# Day 5 수학, 배열
+# 옷가게 할인 받기
+def solution(price):
+    if 100000 <= price < 300000:
+        price = price - price * 0.05
+    elif 300000 <= price < 500000:
+        price = price - price * 0.1
+    elif price >= 500000:
+        price = price - price * 0.2
+    return int(price)
 
+# 아이스 아메리카노
+def solution(money):
+    cnt = exchange = 0
+    cnt = money // 5500
+    exchange = money % 5500
+    return [cnt, exchange]
+
+# 나이 출력
+def solution(age):
+    return 2022 - age + 1
+
+# 배열 뒤집기
+def solution(num_list):
+    return list(reversed(num_list))
+
+def solution(num_list):
+    num_list.reverse()
+    return num_list
+
+def solution(num_list):
+    return num_list[::-1]
