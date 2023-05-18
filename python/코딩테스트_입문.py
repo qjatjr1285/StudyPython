eeeaa7c13f79158883a4569d9d57c16bc65276b7
kim beom seok <qjatjr1285@naver.com>
2023-05-18 17:00:48 +0900

    📝 문제 풀이 추가

--- python/코딩테스트_입문.py
+++ python/코딩테스트_입문.py
@@ -686,5 +686,13 @@ def solution(numbers, direction):
 
 
 # 팩토리얼
+def solution(n):
+    answer = 1
+    factorial = 1
+    while factorial <= n:
+        answer += 1
+        factorial = factorial * answer
+    answer -= 1
+    return answer
 
 
