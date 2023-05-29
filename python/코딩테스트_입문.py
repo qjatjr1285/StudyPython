a86dcf0bb91f1aa28c0f29e51d47e269d34ae4dd
qjatjr1285 <qjatjr1285@naver.com>
2023-05-30 01:51:54 +0900

    📝 영어가 싫어요 풀이 추가

--- python/코딩테스트_입문.py
+++ python/코딩테스트_입문.py
@@ -535,8 +535,17 @@ def solution(my_string):
 
 # Day 15 문자열, 해시, 배열, 수학
 # 영어가 싫어요
+def solution(numbers):
+    numbers = numbers.replace('zero', '0').replace('one', '1').replace('two', '2').replace('three', '3').replace('four', '4').replace('five', '5').replace('six', '6').replace('seven', '7').replace('eight', '8').replace('nine', '9')
+    return int(numbers)
 
+    # 밑에 풀이가 내가 원했던거..
+def solution(numbers):
+    r = {'zero': '0', 'one': '1', 'two': '2', 'three': '3', 'four': '4', 'five': '5', 'six': '6', 'seven': '7', 'eight': '8', 'nine': '9'}
+    for k in r.keys():
+        numbers = numbers.replace(k, r[k])
 
+    return int(numbers)
 
 # 인덱스 바꾸기
 
