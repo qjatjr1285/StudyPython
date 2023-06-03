4a9c921c4527520c3ddde261ec0770a3cb65c809
qjatjr1285 <qjatjr1285@naver.com>
2023-06-03 15:26:09 +0900

    doc: 인덱스 바꾸기 추가

--- python/코딩테스트_입문.py
+++ python/코딩테스트_입문.py
@@ -548,7 +548,10 @@ def solution(numbers):
     return int(numbers)
 
 # 인덱스 바꾸기
-
+def solution(my_string, num1, num2):
+    li = list(my_string)
+    li[num1],li[num2] = li[num2],li[num1]
+    return ''.join(li)
 
 
 # 한 번만 등장한 문자
