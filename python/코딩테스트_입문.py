0dddf22a2d017adbae7c4115c3faf2f0468c502a
qjatjr1285 <qjatjr1285@naver.com>
2023-06-11 18:32:30 +0900

    잘라서 배열로 저장하기 풀이 추가

--- python/코딩테스트_입문.py
+++ python/코딩테스트_입문.py
@@ -732,7 +732,11 @@ def solution(array):
 
 
 # 잘라서 배열로 저장하기
-
+def solution(my_str, n):
+    answer = []
+    for i in range(0, len(my_str), n):
+        answer.append(my_str[i:i+n])
+    return answer
 
 
 # 중복된 숫자 개수
