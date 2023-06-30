476735196c459856c8bc1c36eab9c7dfbe036975
kim beom seok <qjatjr1285@naver.com>
2023-07-01 00:23:16 +0900

    캐릭터의 좌표 풀이 추가

--- python/코딩테스트_입문.py
+++ python/코딩테스트_입문.py
@@ -759,7 +759,20 @@ def solution(dots):
 
 
 # 캐릭터의 좌표
-
+def solution(keyinput, board):
+    col = board[0]
+    row = board[1]
+    result = [0, 0]
+    for i in keyinput:
+        if i == "left" and result[0]-1 >= -(col // 2):
+            result[0] -= 1
+        elif i == "right" and result[0]+1 <= (col // 2):
+            result[0] += 1
+        elif i == "up" and result[1]+1 <= (row // 2):
+            result[1] += 1
+        elif i == "down" and result[1]-1 >= -(row // 2):
+            result[1] -= 1
+    return result
 
 
 # 최댓값 만들기 (2)
