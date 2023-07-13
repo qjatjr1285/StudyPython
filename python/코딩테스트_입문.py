2c28c1e474a897aed5221f592ebe7db2be372b42
kim beom seok <qjatjr1285@naver.com>
2023-07-13 17:03:49 +0900

    안전지대 풀이 추가

--- python/코딩테스트_입문.py
+++ python/코딩테스트_입문.py
@@ -837,7 +837,33 @@ def solution(my_string):
 
 
 # 안전지대
+def solution(board):
+    count = 0
+    row = [-1, 0, 1]
+    col = [-1, 0, 1]
+    danger_zone = [[0] * len(board) for _ in range(len(board))] 
+    danger_zone_index = []
+    for r in row:
+        for c in col:
+            danger_zone_index.append((r, c))
 
+    # 지뢰 팔방을 danger_zone 만들기
+    for i, row in enumerate(board):
+        for j, col in enumerate(row):
+            if board[i][j] == 1:
+                for x, y in danger_zone_index:
+                    try:
+                        if i+x < 0 or j+y < 0 or i+x > len(board) or j+y > len(board):
+                            continue
+                        danger_zone[i+x][j+y] = "x"
+                    except:
+                        continue
+    for i, row in enumerate(danger_zone):
+        for j, col in enumerate(row):
+            if danger_zone[i][j] != "x":
+                count += 1
+
+    return count
 
 
 # 삼각형의 완성조건 (2)
